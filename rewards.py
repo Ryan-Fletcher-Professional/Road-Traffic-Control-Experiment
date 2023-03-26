18bda9e3e223e58f37b76058d16d1112a30487b2
GitUser019203 <126734096+GitUser019203@users.noreply.github.com>
2023-03-26 15:16:00 -0400

    Update rewards.py
    Update reward functions with an impedance-based reward

--- rewards.py
+++ rewards.py
@@ -1,3 +1,5 @@
+from math import e
+
 # Calculates maximum number of waiting and approaching vehicles
 def max_WAVE_reward_fn(traffic_signal):
     max_so_far = 0.0
@@ -10,3 +12,12 @@ def max_WAVE_reward_fn(traffic_signal):
 # Calculates average speed
 def my_reward_fn(traffic_signal):
     return traffic_signal.get_average_speed
+
+# Calculates an impedance-based reward
+def impedance_reward(traffic_signal):
+    beta = 0.5
+    halted_vehicles=sum(traffic_signal.sumo.lane.getLastStepHaltingNumber(lane) for lane in traffic_signal.lanes)
+    approaching_vehicles=sum(traffic_signal.sumo.lane.getLastStepVehicleNumber(lane) for lane in traffic_signal.lanes)
+    outgoing_vehicles=sum(traffic_signal.sumo.lane.getLastStepVehicleNumber(lane) for lane in traffic_signal.out_lanes)
+    vehicle_count=halted_vehicles+approaching_vehicles+outgoing_vehicles
+    return vehicle_count / e ** (beta * traffic_signal.get_average_speed())
