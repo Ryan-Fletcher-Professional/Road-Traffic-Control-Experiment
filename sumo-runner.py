170a19be08ed9031aab9a59eccf5159ef60f1411
GitUser019203 <126734096+GitUser019203@users.noreply.github.com>
2023-04-11 17:27:46 -0400

    Fixed some bugs.

--- sumo-runner.py
+++ sumo-runner.py
@@ -1,5 +1,3 @@
-# use existing python traffic simulator with predefined traffic conditions to do it
-
 import math
 import random
 import tkinter as tk
@@ -27,35 +25,36 @@ import torch.nn.functional as F
 rng = np.random.default_rng(seed=98765)
 random.seed("98765")
 
-root = tk.Tk()
-root.withdraw()
-directory = ""
-directory = filedialog.askdirectory(title="Select Traffic Data Directory")
-files = filedialog.askopenfiles(title="Select Network and Route Files",initialdir=directory)
-if len(files) < 2:
-    print("Less than 2 files.")
-    exit(0)
+if(len(sys.argv) > 2):
+    netfile = sys.argv[1]
+    routefile = sys.argv[2]
+    begin_time_s = int(sys.argv[3])
+else:
+    root = tk.Tk()
+    root.withdraw()
+    directory = ""
+    directory = filedialog.askdirectory(title="Select Traffic Data Directory")
+    files = filedialog.askopenfiles(title="Select Network and Route Files",initialdir=directory)
+    if len(files) < 2:
+        print("Less than 2 files.")
+        exit(0)
+    else:
+        if(files[0].name.endswith("net.xml")):
+            netfile = files[0].name
+            routefile = files[1].name
+        else:
+            netfile = files[1].name
+            routefile=files[0].name
 
 env = sumo_rl.parallel_env(
-                           net_file=files[0].name,
-                           route_file=files[1].name,
-                           out_csv_name=directory+"\\output.csv",
+                           net_file=netfile,
+                           route_file=routefile,
+                           out_csv_name="output\\test-",
                            num_seconds=100000,
-                           use_gui=False,
-                           reward_fn=rewards.impedance_reward,
-                           min_green=30,
-                           max_green=60
+                           begin_time=begin_time_s,
+                           use_gui=True,
+                           reward_fn=rewards.impedance_reward
                            )
-
-# set up matplotlib
-is_ipython = 'inline' in matplotlib.get_backend()
-if is_ipython:
-    # https://ipython.readthedocs.io/en/stable/api/generated/IPython.display.html#IPython.display.display
-    from IPython import display 
-
-# Turn on interactive mode to plot onto the screen
-# https://matplotlib.org/3.1.1/tutorials/introductory/usage.html#sphx-glr-tutorials-introductory-usage-py
-plt.ion()
 
 # if gpu is to be used
 # Compute Unified Device Architecture (CUDA) is an NVIDIA API for general purpose computing on GPUs
@@ -94,13 +93,9 @@ class DQN(nn.Module):
     def forward(self, x):
         h0 = F.relu(self.layer1(x))
         h1 = F.relu(self.layer2(h0))
-        if(h0.size()[0] != 1):
-            randomNums = rng.integers(low=0, high=h0.size()[1]-1, size=h0.size()[1])
-            indices = torch.tensor([randomNums])
-            compressedh0 = torch.gather(h0, dim=1, index=indices)
+        for i in range(1, 10):
+            compressedh0 = torch.mean(h0, 0, True)
             h2 = self.layer3(h1, compressedh0)
-        else:
-            h2 = self.layer3(h1, h0)
         return self.layer4(h2[0])
 
 
@@ -122,16 +117,10 @@ LR = 1e-4
 # Get the number of state observations
 observations = env.reset()
 
-agents_list = [agent for agent in env.agents]
-# Dictionaries containing agent data aren't necessarily consistently ordered.
-# Using agents_list.index(agent) should allow an arbitrary but consistent ordering of the intersections in the output layer of the NN.
-
-#print(observations)
 # Get number of actions from gym action space
 n_actions = np.sum([env.action_space(a).n for a in env.agents])
-#print("\nActions: ", n_actions)
-n_observations = np.sum([len(list(observations.values())[i]) for i in range(len(list(observations.values())))])
-#print("\nObservations ", n_observations)
+
+n_observations = sum([len(observation) for observation in list(observations.values())])
 
 policy_net = DQN(n_observations, n_actions).to(device) # Cast the DQN parameters and buffers and move them to the device
 target_net = DQN(n_observations, n_actions).to(device) # Cast the DQN parameters and buffers and move them to the device
@@ -142,7 +131,7 @@ memory = ReplayMemory(10000)
 
 steps_done = 0
 
-def select_actions(state):
+def select_actions(states):
     global steps_done
     sample = random.random()
     eps_threshold = EPS_END + (EPS_START - EPS_END) * \
@@ -153,78 +142,23 @@ def select_actions(state):
             # t.max(1) will return the largest column value of each row.
             # second column on max result is index of where max element was
             # found, so we pick action with the larger expected reward.
-            net = policy_net(state)
-            #print("\nNet: ", net)
+            net_out = policy_net(states)
             actions = {}
+            
+            agent_action_index = 0
             for agent in env.agents:
-                actions[agent] = 0 if net[0][2*agents_list.index(agent)].item() >= net[0][(2*agents_list.index(agent))+1].item() else 1
-                # CHANGE NN OUTPUT LAYER SIZE TO env.num_agents AND READ +/-???
+                actions_number = env.action_space(agent).n
+                opt_action_index = net_out.numpy()[0][agent_action_index:agent_action_index+actions_number].argmax()
+                agent_action_index += actions_number
+                actions[agent] = opt_action_index            
             return actions
     else:
         actions = {agent: torch.tensor([[env.action_space(agent).sample()]], device=device, dtype=torch.long)[0].item() for agent in env.agents}
-        #print("\nActions: ", actions)
         return actions
 
 
 episode_durations = []
 steps = []
-rewards = []
-
-def plot_durations(show_result=False):
-    plt.figure(1)
-    durations_t = torch.tensor(episode_durations, dtype=torch.float)
-    if show_result:
-        plt.title('Result')
-    else:
-        plt.clf()
-        plt.title('Training...')
-    plt.xlabel('Episode')
-    plt.ylabel('Duration')
-    plt.plot(durations_t.numpy())
-    # Take 100 episode averages and plot them too
-    if len(durations_t) >= 100:
-        means = durations_t.unfold(0, 100, 1).mean(1).view(-1)
-        means = torch.cat((torch.zeros(99), means))
-        plt.plot(means.numpy())
-
-    plt.pause(0.001)  # pause a bit so that plots are updated
-    if is_ipython:
-        if not show_result:
-            display.display(plt.gcf())
-            display.clear_output(wait=True)
-        else:
-            display.display(plt.gcf())
-
-def plot_rewards(show_result=False):
-    plt.figure(2)
-    steps_t = torch.tensor(steps,dtype=torch.int64)
-    rewards_t = torch.tensor([], dtype=torch.float)
-    for reward in rewards:
-        rewards_t = torch.cat((rewards_t, torch.unsqueeze(reward, 0)))
-    if show_result:
-        plt.title('Result')
-    else:
-        plt.clf()
-        plt.title('Training...')
-    plt.xlabel('Step')
-    plt.ylabel('Reward')
-    plt.plot(steps_t.numpy(), rewards_t.numpy())
-    #for i in range(len(rewards_t)):  # Use this to get number labels for the intersections
-        #plt.plot(steps_t.numpy(), rewards_t[i].numpy(), str(i))
-
-    # Take 100 episode averages and plot them too
-    # if len(rewards_t) >= 100:
-    #     means = rewards_t.unfold(0, 100, 1).mean(1).view(-1)
-    #     means = torch.cat((torch.zeros(99), means))
-    #     plt.plot(means.numpy())
-    
-    plt.pause(0.001)  # pause a bit so that plots are updated
-    if is_ipython:
-        if not show_result:
-            display.display(plt.gcf())
-            display.clear_output(wait=True)
-        else:
-            display.display(plt.gcf())
 
 def optimize_model():
     if len(memory) < BATCH_SIZE:
@@ -242,13 +176,13 @@ def optimize_model():
     non_final_next_states = torch.cat([s for s in batch.next_state
                                                 if s is not None])
     state_batch = torch.cat(batch.state)
-    action_batch = torch.cat(batch.action)
-    reward_batch = torch.cat(batch.reward)
+    action_batch = torch.Tensor([list(action_dict.values()) for action_dict in batch.action])
+    reward_batch = torch.Tensor([list(rewards) for rewards in batch.reward])
 
     # Compute Q(s_t, a) - the model computes Q(s_t), then we select the
     # columns of actions taken. These are the actions which would've been taken
     # for each batch state according to policy_net
-    state_action_values = policy_net(state_batch).gather(1, action_batch)
+    state_action_values = policy_net(state_batch).gather(1, action_batch.type(torch.int64))
 
     # Compute V(s_{t+1}) for all next states.
     # Expected values of actions for non_final_next_states are computed based
@@ -259,7 +193,7 @@ def optimize_model():
     with torch.no_grad():
         next_state_values[non_final_mask] = target_net(non_final_next_states).max(1)[0]
     # Compute the expected Q values
-    expected_state_action_values = (next_state_values * GAMMA) + reward_batch               # MAY NEED TO RESHAPE/COMPRESS
+    expected_state_action_values = (next_state_values * GAMMA) + reward_batch.mean(1)            
 
     # Compute Huber loss
     criterion = nn.SmoothL1Loss()
@@ -280,43 +214,26 @@ else:
 
 for i_episode in range(num_episodes):
     # Initialize the environment and get it's state
+
     obs = env.reset()
-    # print("\nObs: ", obs)
-    obs_lists = [[] for i in range(env.num_agents)]
-    for agent in env.agents:  # Sort observations into proper agent ordering
-        obs_lists[agents_list.index(agent)] = obs[agent]
     observations = np.array([], dtype=np.float32)
-    for ob in obs_lists:  # Flatten observations into ordered inputs for NN
-        observations = np.concatenate((observations, ob))
-    # print("\nAgents: ", env.agents)
-    # print("\nObservations flattened: ", observations)
+
+    for agent in env.agents:
+        observations = np.concatenate((observations, obs[agent]), axis=None)
+
     states = torch.tensor(observations, dtype=torch.float32, device=device).unsqueeze(0)
     for t in count():
-        #while env.agents:
+
         actions = select_actions(states)
-        # for agent in env.agents:
-        #     actions[agent] = None#select_action(, agent)
-            # actions = {agent: env.action_space(agent).sample() for agent in env.agents}  # this is where you would insert your policy
         obs, rews, terminations, truncations, infos = env.step(actions)
-        #print("\nRewards: ", rews)
-        obs_lists = [[] for i in range(env.num_agents)]
-        for agent in env.agents:  # Sort observations into proper agent ordering
-            obs_lists[agents_list.index(agent)] = obs[agent]
         observations = np.array([], dtype=np.float32)
-        for ob in obs_lists:  # Flatten observations into ordered inputs for NN
-            observations = np.concatenate((observations, ob))
-        rew_lists = [[] for i in range(env.num_agents)]
         for agent in env.agents:
-                rew_lists[agents_list.index(agent)] = rews[agent]
+            observations = np.concatenate((observations, obs[agent]), axis=None)
         rewards_new = []
-        for r in rew_lists:
-            rewards_new.append(r)
-        #print("\nTerminations: ", terminations)
-        #print("\nTruncations: ", truncations)
+        for agent in env.agents:
+            rewards_new.append(rews[agent])
         terminated = True if True in list(terminations.values()) else False
         truncated = True if True in list(truncations.values()) else False
-        # action = select_action(state)
-        # observation, reward, terminated, truncated, _ = env.step(action.item())
         reward = torch.tensor(rewards_new, device=device)
         done = terminated or truncated
 
@@ -342,18 +259,10 @@ for i_episode in range(num_episodes):
             target_net_state_dict[key] = policy_net_state_dict[key]*TAU + target_net_state_dict[key]*(1-TAU)
         target_net.load_state_dict(target_net_state_dict)
 
-        rewards.append(reward)
         steps.append(steps_done)
-        plot_rewards()
 
         if done:
             episode_durations.append(t + 1)
-            #rewards.append(reward)
-            #steps.append(steps_done)
-            #plot_rewards()             # UNCOMMENT???
             break
 
 print('Complete')
-plot_rewards(show_result=True)
-plt.ioff()
-plt.show()
