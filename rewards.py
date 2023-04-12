5088f4217207a041752b62d44198f056cde7863f
GitUser019203 <126734096+GitUser019203@users.noreply.github.com>
2023-04-12 09:26:38 -0400

    Added new reward functions

--- rewards.py
+++ rewards.py
@@ -14,14 +14,44 @@ def max_WAVE_reward_fn(traffic_signal):
     traffic_signal.last_measure = max_so_far
     return reward
 
+# no reward for fixed time signalling
+def no_reward(traffic_signal):
+    return 0
 
 
 # Calculates average speed
 def my_reward_fn(traffic_signal):
     return traffic_signal.get_average_speed()
 
-# Calculates an impedance-based reward
-def impedance_reward(traffic_signal):
+# Calculates a multi agent impedance-based reward with full coordination among agents
+def fully_coordinated_impedance_reward(traffic_signal):
+    beta = 5
+    max_so_far = 0.0
+   
+    all_traffic_signals = traffic_signal.env.traffic_signals
+    for agent in all_traffic_signals:
+        curr_ts = all_traffic_signals[agent]
+        for lane in curr_ts.lanes:
+            impedance = (curr_ts.sumo.lane.getLastStepHaltingNumber(lane) \
+                + curr_ts.sumo.lane.getLastStepVehicleNumber(lane)) / e ** (beta * curr_ts.get_average_speed())
+            if(impedance > max_so_far):
+                max_so_far = impedance
+
+        for lane in curr_ts.out_lanes:
+            impedance = (curr_ts.sumo.lane.getLastStepVehicleNumber(lane) \
+                + curr_ts.sumo.lane.getLastStepHaltingNumber(lane))  / e ** (beta * curr_ts.get_average_speed())
+            if(impedance > max_so_far):
+                max_so_far = impedance
+
+    if traffic_signal.last_measure < max_so_far:
+        reward = traffic_signal.last_measure - max_so_far
+    else:
+        reward = traffic_signal.last_measure - max_so_far
+    traffic_signal.last_measure = max_so_far
+    return reward
+
+# Calculates a single agent impedance-based reward
+def single_agent_impedance_reward(traffic_signal):
     beta = 5
     max_so_far = 0.0
    
