7a3c7b4ef98c431dcad2bbaf590551f8ddc25f62
GitUser019203 <mathkuzh@yahoo.com>
2023-04-21 10:09:42 -0400

    Updated powershell scripts.

--- powershell/networks.ps1
+++ powershell/networks.ps1
@@ -1,3 +1,3 @@
 # Run Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass in PS to bypass signing and run
-$xml = [XML](gc ..\networks\ingolstadt21.net.xml)
-$xml.net.junction.id -replace ":","" | out-file junctions
+$xml = [XML](gc sumo_networks\ingolstadt21.net.xml)
+$xml.net.junction.id | out-file -Encoding utf8 powershell\junctions.txt
