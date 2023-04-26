96fc983d07428627df088808bf19b8ca26aae607
GitUser019203 <mathkuzh@yahoo.com>
2023-04-26 10:55:23 -0400

    In addition to getting type of output from script arguments, added a new output type and also outputs step number.

--- fixed_time_TSC/fixed_ts.py
+++ fixed_time_TSC/fixed_ts.py
@@ -21,16 +21,7 @@ print("Current Working Directory: " + getcwd())
 if(len(sys.argv) > 1):
     # Requires full file path, not relative file paths
     sumo_cfg = sys.argv[1]
-else:
-    root = tk.Tk()
-    root.withdraw()
-    directory = ""
-    directory = filedialog.askdirectory(title="Select Traffic Data Directory")
-    files = filedialog.askopenfiles(title="Select Network and Route Files",initialdir=directory)
-    if len(files) < 1:
-        print("Less than 1 file.")
-        exit(0)
-    sumo_cfg = files[0].name.replace('/', '\\')
+    output_type = sys.argv[2]
 
 traci.start([sumolib.checkBinary("sumo"), "-c", sumo_cfg, "--begin", "57600"], label="init_connection")
 conn = traci.getConnection("init_connection")
@@ -51,16 +42,31 @@ with open("powershell/junctions.txt", 'r') as junctions:
         traci.junction.subscribeContext(junctionID, tc.CMD_GET_VEHICLE_VARIABLE, 42, [tc.VAR_SPEED, tc.VAR_WAITING_TIME])
         print("Subscription Results for Junction " + junctionID + ": " + str(traci.junction.getContextSubscriptionResults(junctionID)))
 
-with io.open(output_dir + "\\fixed_ts.txt", 'w+') as f:
-    for step in range(4000):
-        print("Step is: " + str(step))
-        traci.simulationStep()
-        vehiclelist = traci.vehicle.getIDList()
-        vehicle_dict = {}
-        for vehicle in vehiclelist:
-            vehicle_dict[vehicle] = traci.vehicle.getAccumulatedWaitingTime(vehicle)
-        str(vehicle_dict)
-        waiting_times = [traci.vehicle.getAccumulatedWaitingTime(vehicle) for vehicle in vehiclelist]
-        f.write(str(sum(waiting_times)))
-        f.write('\n')
+with io.open(output_dir + "\\fixed_ts.txt", 'w+') as f: 
+    if output_type == "--stopped":
+        for step in range(4000):
+            print("Step is: " + str(step))
+            traci.simulationStep()
+            vehiclelist = traci.vehicle.getIDList()
+            vehicle_dict = {}
+            for vehicle in vehiclelist:
+                vehicle_dict[vehicle] = traci.vehicle.getAccumulatedWaitingTime(vehicle)
+            str(vehicle_dict)
+            waiting_times = [traci.vehicle.getAccumulatedWaitingTime(vehicle) for vehicle in vehiclelist]
+            f.write(str(step) + " ")
+            f.write(str(sum(waiting_times)))
+            f.write('\n')
+    elif output_type == "--waitingTime":
+        for step in range(4000):
+            print("Step is: " + str(step))
+            traci.simulationStep()
+            vehiclelist = traci.vehicle.getIDList()
+            vehicle_dict = {}
+            for vehicle in vehiclelist:
+                vehicle_dict[vehicle] = traci.vehicle.isStopped(vehicle)
+            str(vehicle_dict)
+            stopped_vehicles = [vehicle for vehicle in vehiclelist if traci.vehicle.isStopped(vehicle)]
+            f.write(str(step) + " ")
+            f.write(len(stopped_vehicles))
+            f.write('\n')
 traci.close()
