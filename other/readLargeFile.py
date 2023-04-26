569571d04b509eb0875043c6142be31ed0a0ed54
GitUser019203 <mathkuzh@yahoo.com>
2023-04-26 08:47:33 -0400

    Added code to check for EOF.

--- other/readLargeFile.py
+++ other/readLargeFile.py
@@ -1,11 +1,12 @@
 # The file full_output.xml is greater than 3 GB large.
+from os import fstat
 
 first_num_lines = 100
 xml_dir = r"output\fixed_time_ts on 04-25-2023 21-22-27\full_output.xml"
 with open(xml_dir, 'r') as xml_file:
     line = xml_file.readline().strip()
     for i in range(0, first_num_lines):
-        if "" != line:
-            print(line)
-        else:
-            line = xml_file.readline().strip()
+        print(line)
+        line = xml_file.readline().strip()
+        if xml_file.tell() == fstat(xml_file.fileno()).st_size:
+            break
