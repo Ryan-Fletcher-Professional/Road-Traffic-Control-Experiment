75ef78c38823e5fab107bda67cfacc5f861b123a
GitUser019203 <mathkuzh@yahoo.com>
2023-04-26 08:25:00 -0400

    Updated project file, removed SyntaxWarning from readLargeFile and added filterElements tool to the project.

--- other/readLargeFile.py
+++ other/readLargeFile.py
@@ -5,7 +5,7 @@ xml_dir = r"output\fixed_time_ts on 04-25-2023 21-22-27\full_output.xml"
 with open(xml_dir, 'r') as xml_file:
     line = xml_file.readline().strip()
     for i in range(0, first_num_lines):
-        if "" is not line:
+        if "" != line:
             print(line)
         else:
             line = xml_file.readline().strip()
