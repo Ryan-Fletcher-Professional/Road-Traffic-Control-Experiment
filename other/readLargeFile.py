2dfce05a734bdaa042395bfdbd62c45a5cd6d076
GitUser019203 <mathkuzh@yahoo.com>
2023-04-27 07:05:16 -0400

    Added code that parses the large XML file generated by SUMO GUI with the --full-output option. Needs update to produce desired output.

--- other/readLargeFile.py
+++ other/readLargeFile.py
@@ -1,12 +1,28 @@
 # The file full_output.xml is greater than 3 GB large.
 from os import fstat
+import re
 
-first_num_lines = 100
-xml_dir = r"output\fixed_time_ts on 04-25-2023 21-22-27\full_output.xml"
-with open(xml_dir, 'r') as xml_file:
-    line = xml_file.readline().strip()
-    for i in range(0, first_num_lines):
-        print(line)
-        line = xml_file.readline().strip()
-        if xml_file.tell() == fstat(xml_file.fileno()).st_size:
-            break
+vehicle_pattern = re.compile('.*<vehicle.*waiting=.*/>.*', re.I | re.M | re.DOTALL)
+data_pattern = re.compile('.*<data.*timestep=.*>.*', re.I | re.M | re.DOTALL)
+
+
+def readingLargeFile():
+    with open(r"output\fixed_time_ts on 04-25-2023 21-22-27\full_output.xml") as xml_file:
+        num_lines = 0
+        current_step = 1
+        read_bytes = 0
+        while True:
+            line = xml_file.readline()
+            read_bytes += len(line)
+            read_percent = read_bytes / (3.62 * 1024 * 1024 * 1024)
+            line = line.strip()
+            if xml_file.tell() == fstat(xml_file.fileno()).st_size:
+                break
+            if re.search(vehicle_pattern, line):
+                print(line)
+            elif re.search(data_pattern, line):
+                current_step += 1
+                print(line)
+            num_lines = num_lines + 1
+
+readingLargeFile()
