60795af36ad21657deffd18a974c45a6423e6c56
NephilimGreen <ryan.fletcher.9999@gmail.com>
2023-05-07 06:10:28 -0400

    Made episode data folder naming slightly clearer.

--- multi_network_runner_uncompressed_obs.py
+++ multi_network_runner_uncompressed_obs.py
@@ -41,7 +41,7 @@ network_name = net_file[net_file.rindex('\\') + 1:net_file.index('.')]
 
 # Get string representation of current date and time
 now = datetime.now()
-output_dir = getcwd() + "\\output\\" + f"CDQN uncompressed Obs {network_name} " + now.strftime("%m-%d-%Y %H-%M-%S")
+output_dir = getcwd() + "\\output\\" + f"CDQN uncompressed Obs THREE ROUTES {network_name} " + now.strftime("%m-%d-%Y %H-%M-%S")
 mkdir(output_dir)
 
 envs = []
